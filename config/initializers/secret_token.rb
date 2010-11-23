e5878ddf2ce1b1d8b87d5095177f9b29b69a3c72
pesome <pesome@gmail.com>
2010-11-23 09:05:26 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oneday::Application.config.secret_token = 'fba79e321cb1619dccff1eb8f939192559ef5a5fd28495262be7af1f2790027b2664d92e7a9557960e489b5f1f090baaf84b1a0b3908bce9a847c0f44cedec61'
